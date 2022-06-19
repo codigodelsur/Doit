7c0b35f763e71613156f492358385200d1e3b319
Juan Manuel Cicao <jcicao@codigodelsur.com>
2022-06-19 12:17:17 -0300

    feat(task-item): re-implement the item using constraint layout

--- app/src/main/java/com/codigodelsur/doit/presentation/screen/home/TaskItem.kt
+++ app/src/main/java/com/codigodelsur/doit/presentation/screen/home/TaskItem.kt
@@ -1,8 +1,6 @@
 package com.codigodelsur.doit.presentation.screen.home
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
@@ -10,11 +8,11 @@ import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.codigodelsur.doit.presentation.model.PTask
 import com.codigodelsur.doit.presentation.theme.DoitTheme
 import com.codigodelsur.doit.presentation.util.toFormattedString
@@ -28,31 +26,53 @@ fun TaskItem(
         modifier = modifier,
         elevation = 0.dp,
     ) {
-        Column(
+        ConstraintLayout(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
+            val (status, title, description, date) = createRefs()
             Text(
                 text = stringResource(id = task.status.labelRes).uppercase(),
+                modifier = Modifier.constrainAs(status) {
+                    top.linkTo(anchor = parent.top)
+                    start.linkTo(anchor = parent.start)
+                },
                 color = task.status.color,
                 style = MaterialTheme.typography.overline,
             )
             Text(
                 text = task.title,
+                modifier = Modifier.constrainAs(title) {
+                    top.linkTo(
+                        anchor = status.bottom,
+                        margin = 8.dp,
+                    )
+                    start.linkTo(anchor = parent.start)
+                },
                 style = MaterialTheme.typography.h6,
             )
             Text(
                 text = task.description,
+                modifier = Modifier.constrainAs(description) {
+                    top.linkTo(
+                        anchor = title.bottom,
+                        margin = 8.dp,
+                    )
+                    start.linkTo(anchor = parent.start)
+                },
                 color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium),
                 style = MaterialTheme.typography.body1
             )
             Text(
                 text = task.date.toFormattedString(),
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(top = 16.dp),
+                modifier = Modifier.constrainAs(date) {
+                    top.linkTo(
+                        anchor = description.bottom,
+                        margin = 24.dp,
+                    )
+                    end.linkTo(anchor = parent.end)
+                },
                 color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
                 style = MaterialTheme.typography.caption,
             )
